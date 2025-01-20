97d45de4125ecfe85b02827d3e34f24b8f4c6e24
ches4117 <ches4117@gmail.com>
2025-01-20 23:37:14 +0800

    feat: add common tab

--- popup.js
+++ popup.js
@@ -1,31 +1,67 @@
 const $ = window.jQuery;
 
 // Tab Menu
+const tabs = document.querySelectorAll(".tab");
+const searchGallery = document.querySelector(".search-gallery");
+const commonGallery = document.querySelector(".common-gallery");
+const commonList = document.querySelector(".common-list");
+const tabContents = document.querySelectorAll(".tab-content");
 
-const tabs = document.querySelectorAll('.tab');
-const tabContents = document.querySelectorAll('.tab-content');
+chrome.storage.sync.get("commonImageList", (res) => {
+  if (!res.commonImageList) return;
+  res.commonImageList
+    .filter((image) => image.url && image.alt)
+    .forEach((image) => {
+      const imgElement = createImage(image, commonGallery);
+      imgElement.style.cursor = "pointer";
+      imgElement.addEventListener("click", () => {
+        copyImage();
+        imgElement.style.cursor = "default";
+        setTimeout(() => {
+          imgElement.style.cursor = "pointer";
+        }, 200);
+      });
+    });
+});
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    tabs.forEach(t => t.classList.remove('active'));
-    tabContents.forEach(content => content.classList.remove('active'));
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((t) => t.classList.remove("active"));
+    tabContents.forEach((content) => content.classList.remove("active"));
 
-    tab.classList.add('active');
-    document.getElementById(tab.dataset.tab).classList.add('active');
+    tab.classList.add("active");
+    document.getElementById(tab.dataset.tab).classList.add("active");
+    if (tab.dataset.tab !== "searchTab") hideOverlay();
   });
 });
 
 // Search Image
-
-const searchInput = document.getElementById('searchInput');
+const searchInput = document.getElementById("searchInput");
 let searchTimeoutInstance = null;
 
+function createImage(targetImg, tabContent) {
+  const imgContainer = document.createElement("div");
+  imgContainer.setAttribute("class", "image-container");
+
+  const img = document.createElement("img");
+  img.src = targetImg.url;
+  img.alt = targetImg.alt;
+  img.width = 300;
+  img.height = 150;
+  imgContainer.appendChild(img);
+  tabContent.appendChild(imgContainer);
+
+  const altText = document.createElement("span");
+  altText.innerHTML = targetImg.alt;
+  imgContainer.appendChild(altText);
+  return img;
+}
+
 function createSearch(inputValue) {
-  const gallery = document.querySelector('.gallery');
   if (!inputValue) {
-    gallery.innerHTML = '';
-    return
-  };
+    searchGallery.innerHTML = "";
+    return;
+  }
 
   searchTimeoutInstance = setTimeout(function () {
     searchTimeoutInstance = null;
@@ -33,27 +69,17 @@ function createSearch(inputValue) {
     $.ajax({
       url,
     }).done(function (data) {
-      gallery.innerHTML = '';
-      data.urls.forEach((url) => {
-        const imgContainer = document.createElement('div');
-        imgContainer.setAttribute('class', 'image-container');
-
-        const img = document.createElement('img');
-        img.src = url.url;
-        img.alt = url.alt;
-        img.width = 200;
-        img.height = 100;
-        imgContainer.appendChild(img);
-        gallery.appendChild(imgContainer);
-      });
+      searchGallery.innerHTML = "";
+      data.urls.forEach((url) => createImage(url, searchGallery));
     });
   }, 500);
 }
 
-const gallery = document.querySelector('.gallery');
+let hoverImgSrc = null;
+let hoverImgAlt = null;
 // 當滑鼠移入圖片時顯示遮罩
-gallery.addEventListener('mouseover', (e) => {
-  const target = e.target.closest('.image-container');
+searchGallery.addEventListener("mouseover", (e) => {
+  const target = e.target.closest(".image-container").querySelector("img");
   if (target) {
     const rect = target.getBoundingClientRect();
     overlay.style.width = `${rect.width}px`;
@@ -61,32 +87,70 @@ gallery.addEventListener('mouseover', (e) => {
     overlay.style.top = `${rect.top + window.scrollY}px`;
     overlay.style.left = `${rect.left + window.scrollX}px`;
     overlay.style.opacity = 1;
-    overlay.style.pointerEvents = 'auto';
+    overlay.style.pointerEvents = "auto";
 
-    // 更新按鈕行為
-    const imgSrc = target.querySelector('img').src;
-    copyButton.onclick = () => {
-      navigator.clipboard.writeText(imgSrc).then(() => {
-        alert('已複製圖片 URL！');
-      });
-    };
-    saveButton.onclick = () => {
-      const link = document.createElement('a');
-      link.href = imgSrc;
-      link.download = imgSrc.split('/').pop();
-      link.click();
-    };
+    hoverImgSrc = target.src;
+    hoverImgAlt = target.alt;
   }
 });
 
-// 當滑鼠移出時隱藏遮罩
-gallery.addEventListener('mouseout', () => {
-  overlay.style.opacity = 0;
-  overlay.style.pointerEvents = 'none';
+commonGallery.addEventListener("mouseover", (e) => {
+  const target = e.target.closest(".image-container").querySelector("img");
+  if (target) {
+    hoverImgSrc = target.src;
+    hoverImgAlt = target.alt;
+  }
 });
 
-searchInput.addEventListener('input', function () {
+function copyImage() {
+  navigator.clipboard.writeText(hoverImgSrc);
+}
+
+$(".copy-btn").click(() => {
+  copyImage();
+  $(".copy-btn").html("✓");
+  setTimeout(() => {
+    $(".copy-btn").html("複製");
+  }, 500);
+});
+
+$(".save-btn").click(() => {
+  const link = document.createElement("a");
+  link.href = hoverImgSrc;
+  link.download = hoverImgSrc.split("/").pop();
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  delete link;
+});
+
+$(".common-btn").click(() => {
+  // Save the img object
+  chrome.storage.sync.get(["commonImageList"], function (result) {
+    const hoverImg = { url: hoverImgSrc, alt: hoverImgAlt };
+    const repos = result.commonImageList || [];
+    repos.push(hoverImg);
+    chrome.storage.sync.set({ commonImageList: repos });
+  });
+});
+
+function hideOverlay() {
+  overlay.style.opacity = 0;
+  overlay.style.pointerEvents = "none";
+}
+
+// 當滑鼠移出時隱藏遮罩
+searchGallery.addEventListener("mouseout", (event) => {
+  if (event.target.tagName === "IMG") return;
+  hideOverlay();
+});
+
+searchInput.addEventListener("input", function () {
   inputValue = searchInput.value.trim();
+  if (!inputValue) {
+    hideOverlay();
+  }
+
   if (searchTimeoutInstance) {
     clearTimeout(searchTimeoutInstance);
     createSearch(inputValue);
